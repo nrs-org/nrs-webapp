4dc01ddc6b756e3c8ef3a22f318cff1d56e902c0
btmxh <ngoduyanh.chip@gmail.com>
2025-12-26 19:20:10 +0700

    chore: remove unused quickdev test

--- nrs-webapp/tests/test.rs
+++
@@ -1,7 +0,0 @@
-#[tokio::test]
-async fn test_test() -> anyhow::Result<()> {
-    let hc = httpc_test::new_client("http://localhost:3621")?;
-
-    hc.do_get("/test").await?.print().await?;
-    Ok(())
-}
