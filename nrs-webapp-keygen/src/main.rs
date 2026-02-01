583387cdf97dd49d03886ceb2b9316724d20063b
btmxh <ngoduyanh.chip@gmail.com>
2026-02-01 17:15:53 +0900

    fix(keygen): printing only 8 out of 128 bytes

--- nrs-webapp-keygen/src/main.rs
+++ nrs-webapp-keygen/src/main.rs
@@ -12,7 +12,19 @@ fn main() {
     const KEY_LENGTH: usize = 128;
 
     let key: [u8; KEY_LENGTH] = rand::random();
-    eprintln!("Generated key: {:?}", key);
+
+    // print only 8 bytes, censoring the rest for security
+    let prefix = 8;
+    let mut key_str = vec![];
+    for (i, byte) in key.iter().enumerate() {
+        if i < prefix {
+            key_str.push(format!("{:02x}", byte));
+        } else if i == prefix {
+            key_str.push("**".into());
+            break;
+        }
+    }
+    eprintln!("Generated key: {:?}", key_str);
 
     let base64_key = BASE64_URL_SAFE.encode(key);
     println!("{}", base64_key);
