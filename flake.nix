b7066b1aa0d7c97763c8bf6b18882901379eeea8
btmxh <ngoduyanh.chip@gmail.com>
2026-02-01 17:02:59 +0900

    fix: fix flake.nix file

--- flake.nix
+++ flake.nix
@@ -3,27 +3,51 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    jailed-agents.url = "github:btmxh/jailed-agents";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      nixpkgs,
+      jailed-agents,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustc
-          cargo
-          rustfmt
-          clippy
-          pnpm
-          nodejs
-          bacon
-          lld
-          wasm-bindgen-cli
-        ];
+        packages =
+          with pkgs;
+          [
+            rustc
+            cargo
+            rustfmt
+            clippy
+            pnpm
+            nodejs
+            bacon
+            lld
+            wasm-bindgen-cli
+
+            # nix stuff
+            nixfmt
+            nixd
+          ]
+          ++ (
+            builtins.attrValues (
+              jailed-agents.lib.${system}.makeJailedAgents {
+                extraPkgs = with pkgs; [
+                  rustc
+                  cargo
+                  rustfmt
+                  clippy
+                  wasm-bindgen-cli
+                ];
+              }
+            )
+          );
       };
     };
 }
