e8068bb84ee0d1de6f95fc0d1adcf5e66fddd071
btmxh <ngoduyanh.chip@gmail.com>
2026-02-01 23:20:00 +0900

    ci: add pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -4,52 +4,79 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     jailed-agents.url = "github:btmxh/jailed-agents";
-    flake-utils.url = "github:numtide/flake-utils";
+    systems.url = "github:nix-systems/default";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
     {
+      self,
       nixpkgs,
       jailed-agents,
-      flake-utils,
+      systems,
+      git-hooks,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages =
-            with pkgs;
-            [
-              rustc
-              cargo
-              rustfmt
-              clippy
-              pnpm
-              nodejs
-              bacon
-              lld
-              wasm-bindgen-cli
-
-              # nix stuff
-              nixfmt
-              nixd
-            ]
-            ++ (builtins.attrValues (
-              jailed-agents.lib.${system}.makeJailedAgents {
-                extraPkgs = with pkgs; [
-                  rustc
-                  cargo
-                  rustfmt
-                  clippy
-                  wasm-bindgen-cli
-                ];
-              }
-            ));
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      checks = forEachSystem (system: {
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt.enable = true;
+            statix.enable = true;
+            check-yaml.enable = true;
+            end-of-file-fixer.enable = true;
+            trim-trailing-whitespace.enable = true;
+            rustfmt.enable = true;
+          };
         };
-      }
-    );
+      });
+
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+        in
+        {
+          default = pkgs.mkShell {
+            inherit shellHook;
+            buildInputs = enabledPackages;
+
+            packages =
+              with pkgs;
+              [
+                rustc
+                cargo
+                rustfmt
+                clippy
+                pnpm
+                nodejs
+                bacon
+                lld
+                wasm-bindgen-cli
+
+                # nix stuff
+                nixfmt
+                nixd
+              ]
+              ++ (builtins.attrValues (
+                jailed-agents.lib.${system}.makeJailedAgents {
+                  extraPkgs = with pkgs; [
+                    rustc
+                    cargo
+                    rustfmt
+                    clippy
+                    wasm-bindgen-cli
+                  ];
+                }
+              ));
+          };
+        }
+      );
+
+    };
 }
