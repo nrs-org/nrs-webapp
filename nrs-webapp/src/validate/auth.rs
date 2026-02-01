519d2fc14c6a6a70b7f843fe9972691d29d26353
btmxh <ngoduyanh.chip@gmail.com>
2026-02-01 18:35:48 +0900

    fix: fix ValidationError::new bs

--- nrs-webapp/src/validate/auth.rs
+++ nrs-webapp/src/validate/auth.rs
@@ -18,19 +18,16 @@ pub static USERNAME_REGEX: LazyRegex = lazy_regex!(r"^[A-Za-z0-9_\-]{3,20}$");
 /// ```
 pub fn validate_password(password: &str) -> Result<(), ValidationError> {
     if !password.chars().any(|c| c.is_ascii_lowercase()) {
-        return Err(ValidationError::new(
-            "Password must contain at least one lowercase letter",
-        ));
+        return Err(ValidationError::new("password_at_least_one_lowercase")
+            .with_message("Password must contain at least one lowercase letter".into()));
     }
     if !password.chars().any(|c| c.is_ascii_uppercase()) {
-        return Err(ValidationError::new(
-            "Password must contain at least one uppercase letter",
-        ));
+        return Err(ValidationError::new("password_at_least_one_uppercase")
+            .with_message("Password must contain at least one uppercase letter".into()));
     }
     if !password.chars().any(|c| c.is_ascii_digit()) {
-        return Err(ValidationError::new(
-            "Password must contain at least one digit",
-        ));
+        return Err(ValidationError::new("password_at_least_one_digit")
+            .with_message("Password must contain at least one digit".into()));
     }
 
     Ok(())
